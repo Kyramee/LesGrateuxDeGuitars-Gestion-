c1c06de98fbd40d2589412c8d08e7dc168ecd737
Kyramee <adrienthereader@gmail.com>
2018-04-18 17:31:00 -0400

    livrable 1 non tester non complet

--- LesArtistesG/src/connexionControler.java
+++ LesArtistesG/src/connexionControler.java
@@ -1,17 +1,31 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class connexionControler {
 	private final static String url = "jdbc:mysql://localhost/LesArtistesG";
 	
-	public static Connection getConnexion() throws SQLException, ClassNotFoundException {
+	private static Connection getConnexion() throws SQLException, ClassNotFoundException {
 		Class.forName("org.gjt.mm.mysql.Driver"); 
 		return DriverManager.getConnection(url,"root","mysql");
 	}
 	
-	public static void closeConnexion(Connection connexion) throws SQLException, ClassNotFoundException {
+	private static void closeConnexion(Connection connexion) throws SQLException, ClassNotFoundException {
 		Class.forName("org.gjt.mm.mysql.Driver"); 
 		connexion.close();
+	}
+	
+	public static ResultSet executerRequete(String requete) throws ClassNotFoundException, SQLException {
+		Connection connexion = getConnexion();
+
+		Statement statement = connexion.createStatement();
+		
+		ResultSet rs = statement.executeQuery(requete);
+		
+		closeConnexion(connexion);
+		
+		return rs;
 	}
 }
--- LesArtistesG/src/gestionArtistes.java
+++ LesArtistesG/src/gestionArtistes.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
@@ -15,22 +13,28 @@ public class gestionArtistes {
 	}
 
 	public ArrayList<Artiste> getTabArtiste() {
+		return getTabArtiste("", "", false);
+	}
+
+	public ArrayList<Artiste> getTabArtiste(String id, String nom, Boolean membre) {
 		this.tabArtiste = new ArrayList<>();
-		String requete = "SELECT * FROM Artiste ORDER BY nom";
+		String requete = "SELECT * FROM Artiste WHERE membre = " + membre;
+
+		if (!id.isEmpty()) {
+			requete += " id = " + id;
+		}
+
+		if (!nom.isEmpty()) {
+			requete += " nom LIKE '%" + nom + "%'";
+		}
 
 		try {
-			Connection connexion = connexionControler.getConnexion();
-
-			Statement statement = connexion.createStatement();
-
-			ResultSet result = statement.executeQuery(requete);
+			ResultSet result = connexionControler.executerRequete(requete);
 
 			while (result.next()) {
 				this.tabArtiste.add(new Artiste(result.getInt("id"), result.getString("nom"),
 						result.getBoolean("membre"), result.getString("photo_url")));
 			}
-
-			connexionControler.closeConnexion(connexion);
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Erreur: " + e, "Erreur", JOptionPane.ERROR_MESSAGE);
 		}
@@ -40,19 +44,62 @@ public class gestionArtistes {
 	public ArrayList<Album> getTabAlbum(int idArtist) {
 		this.tabAlbum = new ArrayList<>();
 		String requete = "SELECT * FROM Album ORDER BY nom WHERE artiste_id = " + idArtist;
-		
+
 		try {
-			Connection connexion = connexionControler.getConnexion();
-
-			Statement statement = connexion.createStatement();
-
-			ResultSet result = statement.executeQuery(requete);
+			ResultSet result = connexionControler.executerRequete(requete);
 
 			while (result.next()) {
-				this.tabAlbum.add(new Album(result.getInt("id"), result.getString("titre"), result.getDouble("prix"), result.getInt("genre_id"), result.getString("annee_sortie"), result.getString("maison_distribution"), result.getString("image_url"), result.getInt("artist_id")));
+				this.tabAlbum.add(new Album(result.getInt("id"), result.getString("titre"), result.getDouble("prix"),
+						result.getInt("genre_id"), result.getString("annee_sortie"),
+						result.getString("maison_distribution"), result.getString("image_url"),
+						result.getInt("artist_id")));
 			}
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Erreur: " + e, "Erreur", JOptionPane.ERROR_MESSAGE);
+		}
+		return this.tabAlbum;
+	}
 
-			connexionControler.closeConnexion(connexion);
+	public void ajouterArtiste(String nom, boolean membre, String photoUrl) {
+		String requete = "INSERT INTO Artiste('id', 'nom', 'membre', 'photo_url') VALUES (null, '" + nom + "', '"
+				+ membre + "', '" + photoUrl + "')";
+
+		try {
+			connexionControler.executerRequete(requete);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Erreur: " + e, "Erreur", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	public void ajouterAlbum(String titre, double prix, int genre, String anneeSortie, String maisonDistribution,
+			String imageUrl, int idArtiste) {
+		String requete = "INSERT INTO Album(id, titre, prix, genre_id, annee_sortie, maison_distribution, image_url, artiste_id) VALUES (null, '"
+				+ titre + "', '" + prix + "', '" + genre + "', '" + anneeSortie + "', '" + maisonDistribution + "', '"
+				+ imageUrl + "', '" + idArtiste + "', )";
+		
+		try {
+			connexionControler.executerRequete(requete);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Erreur: " + e, "Erreur", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	public void modifierArtiste(int id, String nom, boolean membre, String photoUrl) {
+		String requete = "UPDATE Artiste SET nom = '" + nom + ", membre = '" + membre + "', photo_url = '" + photoUrl + "' WHERE id = " + id;
+		
+		try {
+			connexionControler.executerRequete(requete);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Erreur: " + e, "Erreur", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	
+	public void supprimer(String table, int id) {
+		String requete = "DELETE FROM " + table + " WHERE id = " + id;
+		
+		try {
+			connexionControler.executerRequete(requete);
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Erreur: " + e, "Erreur", JOptionPane.ERROR_MESSAGE);
 		}
